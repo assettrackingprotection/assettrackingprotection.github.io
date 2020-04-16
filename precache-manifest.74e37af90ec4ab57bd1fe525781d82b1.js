94ebd04634a3342281a689ea198948c1e28e5171
ethanswatson <ethanswatson@aggiemail.usu.edu>
2020-04-16 21:32:06 +0000

    Deploying to test from  - 05465084bb2ebf9158c1fc6434670224a4469af2 🚀

---
+++ precache-manifest.74e37af90ec4ab57bd1fe525781d82b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950e116b86b243493a9adf05b6fdc7ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8346a789c73cf37734f",
+    "url": "/static/css/main.2ff0e4dc.chunk.css"
+  },
+  {
+    "revision": "97344958d307aa3137d0",
+    "url": "/static/js/2.1989046a.chunk.js"
+  },
+  {
+    "revision": "8b3869c6f4e0531efecd01d78df4f1a5",
+    "url": "/static/js/2.1989046a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8346a789c73cf37734f",
+    "url": "/static/js/main.2b4f93ad.chunk.js"
+  },
+  {
+    "revision": "1a0fb63bc6fbbe3b4b89",
+    "url": "/static/js/runtime-main.b153aeed.js"
+  }
+]);
