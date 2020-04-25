9e522794b3055666be2312048f68ba78a962b1c4
ethanswatson <ethanswatson@aggiemail.usu.edu>
2020-04-25 20:37:45 +0000

    Deploying to master from  - ea8c9b6cde9c14de78be3dc658e13a3fcf6fcd4d 🚀

---
+++ precache-manifest.bfe621032624604044559b9bba3468e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8604b5de9465b428b6501856234b1580",
+    "url": "https://assettrackingprotection.github.io/index.html"
+  },
+  {
+    "revision": "58a56e08aa567c431415",
+    "url": "https://assettrackingprotection.github.io/static/css/main.270be63b.chunk.css"
+  },
+  {
+    "revision": "7fceea72e1e932d5fb75",
+    "url": "https://assettrackingprotection.github.io/static/js/2.0dfc031d.chunk.js"
+  },
+  {
+    "revision": "0c3d3cb5c6515a2b1ad6569491662182",
+    "url": "https://assettrackingprotection.github.io/static/js/2.0dfc031d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a56e08aa567c431415",
+    "url": "https://assettrackingprotection.github.io/static/js/main.1890bece.chunk.js"
+  },
+  {
+    "revision": "bf6cd4221e05de2478f6",
+    "url": "https://assettrackingprotection.github.io/static/js/runtime-main.719ee88f.js"
+  },
+  {
+    "revision": "6d691fab3cb1aa2963a3e74c9cbb9b19",
+    "url": "https://assettrackingprotection.github.io/static/media/profile_icon.6d691fab.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ab9d92e2bb55a2df6ad137297033526.js"
+  "https://assettrackingprotection.github.io/precache-manifest.bfe621032624604044559b9bba3468e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://assettrackingprotection.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
